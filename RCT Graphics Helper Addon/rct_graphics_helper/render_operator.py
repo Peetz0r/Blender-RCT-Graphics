dc250a3b0facd176f93c09fb68ee4721181ffa16
Peetz0r <peter@haas-en-berg.nl>
2020-07-09 04:46:48 +0200

    Hacky workaround for blender crashing
    makes UI freeze during render
    no big deal for intended batch usage

--- RCT Graphics Helper Addon/rct_graphics_helper/render_operator.py
+++ RCT Graphics Helper Addon/rct_graphics_helper/render_operator.py
@@ -19,47 +19,48 @@ class RCTRender(object):
     rendering = False
     stop = False
     renderTask = None
-    
+
     @classmethod
     def poll(cls, context):
         return bpy.data.objects['Rig'] is not None
 
     def pre(self, dummy):
         self.rendering = True
-    
+
     def post(self, dummy):
         self.rendering = False
-    
+
     def cancelled(self, dummy):
         self.stop = True
-    
+
     def execute(self, context):
         rotate_rig(context, 0, 0, 0, 0)
         bpy.data.cameras["Camera"].ortho_scale = 169.72 / (100 / bpy.data.scenes['Scene'].render.resolution_percentage)
-        
+
         self.rendering = False
         self.stop = False
-        
+
         bpy.app.handlers.render_pre.append(self.pre)
         bpy.app.handlers.render_post.append(self.post)
         bpy.app.handlers.render_cancel.append(self.cancelled)
         self._timer = context.window_manager.event_timer_add(0.5, context.window)
-        context.window_manager.modal_handler_add(self)
+        while self.renderTask is not None and self.renderTask.status != "FINISHED":
+            self.renderTask.step()
 
         return {"RUNNING_MODAL"}
-      
-    def modal(self, context, event):  
+
+    def modal(self, context, event):
         if event.type == 'TIMER':
-            
+
             if self.stop or self.renderTask == None or (self.renderTask.status == "FINISHED" and not self.rendering):
                 self.finished(context)
-                
+
                 return {"FINISHED"}
-            
+
             elif not self.rendering:
                 # render next frame
                 self.renderTask.step()
-                
+
         return {"PASS_THROUGH"}
 
     def finished(self, context):
@@ -67,6 +68,6 @@ class RCTRender(object):
         bpy.app.handlers.render_post.remove(self.post)
         bpy.app.handlers.render_cancel.remove(self.cancelled)
         context.window_manager.event_timer_remove(self._timer)
-                
+
         rotate_rig(context, 0, 0, 0, 0)
         bpy.data.cameras["Camera"].ortho_scale = 169.72
